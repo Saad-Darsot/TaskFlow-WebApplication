ab94e5608513f271b067f5b0b99280d90848d317
hsm-msd <hmsadok1302@conestogac.on.ca>
2023-12-12 14:44:56 -0500

    some chnages made for the taskPage (main page), the user Profile place and the username now are showing up in the left side panel

--- TaskFlowWebApp/TaskFlowWebApp/TaskPage.aspx
+++ TaskFlowWebApp/TaskFlowWebApp/TaskPage.aspx
@@ -2,8 +2,9 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <div id="rcorners2">
-        <div id="rcorners3" style="float:left" >
-             This is Trial
+        <div id="rcorners3" style="float:left; display: flex; flex-direction: column; align-items: center;" >
+            <div id="profilePic" style="width:100px;height:100px;border-radius:50%;border:1px solid #000; background-color: grey;"></div>
+            <div id="username" style="margin-top:10px;"><asp:Label ID="username" runat="server" Text=""></asp:Label></div>
         </div>
         <div id="stylebox">
 
--- TaskFlowWebApp/TaskFlowWebApp/TaskPage.aspx.cs
+++ TaskFlowWebApp/TaskFlowWebApp/TaskPage.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,29 @@ namespace TaskFlowWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Use the connection string from web.config
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Taskflow"].ConnectionString;
+            string userEmail = Session["UserEmail"].ToString();
 
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Query to get the username
+                string query = "SELECT username FROM users WHERE email = @email"; // replace with your condition
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@email", userEmail); // replace "useremail" with the actual user's email
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            username.Text = reader["username"].ToString();
+                        }
+                    }
+                }
+            }
         }
     }
 }
